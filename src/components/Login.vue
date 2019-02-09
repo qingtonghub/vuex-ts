6905857b1d9c38b87d6acb7d10c1882f646b3087
qingtonghub <865012773@qq.com>
2019-02-09 21:10:19 +0800

    test user login api

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,38 +3,82 @@
  * @Author: QingTong
  * @Date: 2019-01-25 15:16:27
  * @Last Modified by: qingtong
- * @Last Modified time: 2019-02-08 20:53:37
+ * @Last Modified time: 2019-02-09 21:02:48
  */
 <template>
   <div :class="[$style['page-wrap'], $style['login-wrap']]">
     <div :class="$style['form-box']">
-      
+      <div :class="$style.logo">
+        <img src="../assets/imgs/logo.png" alt="">
+      </div>
+      <div :class="$style.fields">
+        <div :class="$style['form-field']">
+          <label>
+            <input v-model="form.phone" type="text" placeholder="请输入手机号">
+          </label>
+        </div>
+        <div :class="$style['form-field']">
+          <label>
+            <input v-model="form.password" type="password" placeholder="输入密码">
+          </label>
+        </div>
+      </div>
+      <div :class="$style['error-box']">
+        <span>请输入真实的手机号</span>
+      </div>
+      <div :class="$style['btn-box']">
+        <button @click="login">登  录</button>
+      </div>
+      <div :class="$style.extra">
+        <div :class="$style['extra-left']">
+          <router-link to="/forget">忘记密码？</router-link>
+        </div>
+        <div :class="$style['extra-right']">
+          没有账号？现在就
+          <router-link to="/register">注册</router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
   import { Component, Vue } from 'vue-property-decorator';
+  interface Form {
+    phone: string;
+    password: string;
+  }
   @Component({
     name: 'login',
   })
   export default class Login extends Vue {
-    private created(): void {
-      // https://api.github.com/users/octocat/gists
-      // this.axios.get('/clientPool/GetChannel').then((response) => {
-      // this.axios.get('/example/get').then((response) => {
-      //   console.log(response);
-      // });
+    private form: Form = {
+      phone: '',
+      password: '',
+    };
+    private login(): void {
+      console.log(this.form.phone);
+      console.log(this.form.password);
+      if (this.form.phone && this.form.password) {
+        this.axios.post('/user/login', this.form).then((response) => {
+          console.log(response);
+        });
+      }
     }
-    private mounted(): void {
-      // console.log('login mounted');
-    }
-    private activated(): void {
-      console.log('login activated');
-    }
-    private deactivated(): void {
-      console.log('login deactivated');
-    }
+    // private created(): void {
+    //   this.axios.post('/user/post').then((response) => {
+    //     console.log(response);
+    //   });
+    // }
+    // private mounted(): void {
+    //   // console.log('login mounted');
+    // }
+    // private activated(): void {
+    //   console.log('login activated');
+    // }
+    // private deactivated(): void {
+    //   console.log('login deactivated');
+    // }
   }
 </script>
 
@@ -57,12 +101,95 @@
   }
   .form-box {
     width: 344px;
-    height: 320px;
+    // height: 320px;
     position: absolute;
     left: 50%;
     top: 40%;
     z-index: 10;
     transform: translate(-50%, -50%);
-    background: rgba(0, 0, 0, 0.5);
+    // background: rgba(0, 0, 0, 0.5);
+    .logo {
+      display: block;
+      overflow: hidden;
+      text-align: center;
+      margin: auto;
+      margin-bottom: 59px;
+    }
+    .fields {
+      .form-field {
+        height: 44px;
+        &:first-child input{
+          border-top-left-radius: 5px;
+          border-top-right-radius: 5px;
+        }
+        &:last-child input{
+          border-bottom-left-radius: 5px;
+          border-bottom-right-radius: 5px;
+        }
+      }
+      input {
+        display: block;
+        width: 100%;
+        height: 44px;
+        padding: 10px 20px;
+        outline: none;
+        border: none;
+        border-bottom: 1px solid #dae1e9;
+        color: #384655;
+        background-image: none;
+        background-color: #fff;
+        font-size: 14px;
+      }
+    }
+    .error-box {
+      padding: 14px 8px;
+      margin-top: 10px;
+      position: relative;
+      border-radius: 5px;
+      overflow: hidden;
+      text-align: center;
+      color: #fff;
+      display: none;
+    }
+    .btn-box button{
+      display: block;
+      margin-top: 30px;
+      height: 44px;
+      line-height: 44px !important;
+      font-size: 18px !important;
+      color: #fff;
+      background-color: #16d3d0;
+      outline: none;
+      border: none;
+      border-radius: 5px;
+      width: 100%;
+      text-align: center;
+      transition: all .2s ease;
+      cursor: pointer;
+      &:hover {
+        background-color: #12c7c3;
+      }
+    }
+    .extra {
+      overflow: hidden;
+      color: #fff;
+      margin-top: 10px;
+      .extra-left {
+        float: left;
+      }
+      .extra-right {
+        float: right;
+        a {
+          color: #16d3d0;
+        }
+      }
+      a {
+        text-decoration: none;
+        color: #fff;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
   }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
  * @Author: QingTong
  * @Date: 2019-01-31 11:55:02
  * @Last Modified by: qingtong
- * @Last Modified time: 2019-02-08 20:21:24
+ * @Last Modified time: 2019-02-09 19:51:00
  */
 
 module.exports = {
@@ -12,12 +12,12 @@ module.exports = {
   },
   devServer: {
     port: 8081,
-    // proxy: {
-    //   '/': {
-    //     target: 'http://localhost:3001/',
-    //     // changeOrigin: true,
-    //     ws: false,
-    //   }
-    // }
+    proxy: {
+      '/': {
+        target: 'http://localhost:3001/',
+        changeOrigin: true,
+        ws: false,
+      }
+    }
   }
 }
